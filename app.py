317b6dd693ce3e7e045e08ce2f622e4104b181b7
Carlos Prudencio <carlos.pru11@gmail.com>
2025-03-10 16:08:10 -0400

    Exclude innecessary dependecies for prod. Change port

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ from controllers import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=3030, debug=True)
+    app.run(host="0.0.0.0", port=3035, debug=True)
