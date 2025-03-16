697093bc3599cc65b03817ba5e029360b629fbd8
Carlos Prudencio <carlos.pru11@gmail.com>
2025-03-16 19:19:50 -0400

    Implement WSGI Waitress to deploy a web server

--- app.py
+++ app.py
@@ -3,5 +3,8 @@ from controllers import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=3030, debug=True)
+    # app.run(host="0.0.0.0", port=3030, debug=True)
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=3030)
     # ssl_context=("certs/cert.pem", "certs/key.pem")
+    # url_scheme="https", ssl_context=("certs/cert.pem", "certs/key.pem")
